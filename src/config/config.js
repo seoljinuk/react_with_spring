fc8664232a994aa7f8ba1010cc2d89903e5571b9
bluesky <bluesky@example.com>
2025-09-15 17:47:45 +0900

    react 2025/09/15

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 // 현재 파일 내에서만 API_HOST, API_PORT를 사용할 때
 //const API_HOST = "158.179.161.204";
-const API_HOST = "localhost";
+const API_HOST = "localhost"; // 컴퓨터의 ip 주소
 const API_PORT = "9000";
 
 //export const API_HOST = "158.180.70.107";
